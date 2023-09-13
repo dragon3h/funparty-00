f388d6af6e5c02b36a84f43e77b7d70b66e86242
dragon3h <dragon3h@gmx.com>
2023-09-12 20:02:18 -0400

    added html-webpack-plugin and progressplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[main].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new webpack.ProgressPlugin(),    
+    new HtmlWebpackPlugin({
+      title: 'Funparty',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
 };
