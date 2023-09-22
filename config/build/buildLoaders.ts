27844458c90f6e2bbb21f6f16e5845c7e0593689
dragon3h <dragon3h@gmx.com>
2023-09-22 10:19:23 -0400

    build: added css modules loader options

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,27 +3,31 @@ import { RuleSetRule } from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders(options: BuildOptions): RuleSetRule[] {
-  const {isDev} = options;
+  const { isDev } = options;
   const typescriptLoader: RuleSetRule = {
     test: /\.tsx?$/,
-    use: 'ts-loader',
+    use: "ts-loader",
     exclude: /node_modules/,
   };
 
   const cssLoader: RuleSetRule = {
     test: /\.s[ac]ss$/,
-    use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-    {
-      loader:'css-loader',
-      options: {
-        modules: true,
-      }      
-    },
-    'sass-loader'],
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
   };
 
-  return [
-    typescriptLoader,
-    cssLoader
-  ];
+  return [typescriptLoader, cssLoader];
 }
