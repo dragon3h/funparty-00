7fbe6a11657954fa340f88ea90f398aeeca5153f
dragon3h <dragon3h@gmx.com>
2023-10-04 08:46:44 -0400

    feat: created Router provider and config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,27 @@
-import { FC, Suspense, lazy, useContext, useState } from "react";
+import { FC } from "react";
 import "./styles/index.scss";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider/index";
 import { classNames } from "shared/lib/classNames/classNames";
-const Home = lazy(() => import("pages/HomePage/index"));
-const About = lazy(() => import("pages/AboutPage/index"));
+import { AppRouter } from "./providers/router";
 
 const App: FC = () => {
-  const {theme, switchTheme} = useTheme();
+  const { theme, switchTheme } = useTheme();
 
   return (
-    <main className={classNames('app', {hovered: false, sdffdsdf: true}, [theme, 'abc', '222'])}>
+    <main
+      className={classNames("app", { hovered: false, sdffdsdf: true }, [
+        theme,
+        "abc",
+        "222",
+      ])}
+    >
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
       <button onClick={switchTheme}>Switch Theme</button>
-      <Suspense fallback="<div>Loading...</div>">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </main>
   );
 };
