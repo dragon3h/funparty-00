551f810b75ab0b93dbdaaa318fc0d7ae03a3312e
dragon3h <dragon3h@gmx.com>
2023-10-11 08:58:34 -0400

    feat: added tests for the classNames function

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,21 @@
 import { classNames } from './classNames';
 
 describe('classNames', () => {
-  test('test', () => {
-    expect(classNames('testClass')).toBe('testClass');
+  test('with only first param', () => {
+    const expected = 'testClass';
+
+    expect(classNames('testClass')).toBe(expected);
+  });
+
+  test('with cls and additinal', () => {
+    const expected = 'testClass additinal';
+
+    expect(classNames('testClass', {}, ['additinal'])).toBe(expected);
+  });
+
+  test('with all params', () => {
+    const expected = 'testClass additinal mod1';
+
+    expect(classNames('testClass', { mod1: true, mod2: false, }, ['additinal'])).toBe(expected);
   });
 });
